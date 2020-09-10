8ed68e5779d0ccca73170ceb76bfb83a0cff2e39
Daniel Porteous <danielporteous1@gmail.com>
2020-09-10 20:10:39 +1000

    [scripts] Run isort and black

--- scripts/scrape_signbank.py
+++ scripts/scrape_signbank.py
@@ -23,8 +23,8 @@ with this format:
 
 import argparse
 import asyncio
-import logging
 import json
+import logging
 import string
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
@@ -32,7 +32,6 @@ from typing import Dict, List, Tuple
 
 import requests
 from bs4 import BeautifulSoup
-
 
 LOG = logging.getLogger(__name__)
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
@@ -65,7 +64,9 @@ class Word:
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--debug", action="store_true")
-    parser.add_argument("--urls", nargs="*", help="Specific URLs to look at")
+    urls_args = parser.add_mutually_exclusive_group()
+    urls_args.add_argument("--urls", nargs="*", help="Specific URLs to look at")
+    urls_args.add_argument("--urls-file", help="File containing specific URLs to look at")
     parser.add_argument("--letters", nargs="*", help="Fetch only these letters")
     output_args = parser.add_mutually_exclusive_group(required=True)
     output_args.add_argument("--output-file")
@@ -92,8 +93,7 @@ async def get_word_page_urls(executor, letters=None) -> List[str]:
 
     # Get the HTML for the first page of each letter.
     first_letter_pages_urls = [
-        LETTER_PAGE_TEMPLATE.format(letter=letter, page=1)
-        for letter in letters
+        LETTER_PAGE_TEMPLATE.format(letter=letter, page=1) for letter in letters
     ]
     first_letter_pages_html = await get_pages_html(executor, first_letter_pages_urls)
 
@@ -102,7 +102,7 @@ async def get_word_page_urls(executor, letters=None) -> List[str]:
     for idx, html in enumerate(first_letter_pages_html):
         letter = letters[idx]
 
-        soup = BeautifulSoup(html.text, 'html.parser')
+        soup = BeautifulSoup(html.text, "html.parser")
         pages_list = soup.find_all("ul")[-1]
         last_pages_button = pages_list.find_all("li")[-1]
         try:
@@ -129,7 +129,7 @@ async def get_word_page_urls(executor, letters=None) -> List[str]:
     # Get the word URLs from all the letter pages' HTML.
     word_page_urls = []
     for html in letter_pages_html:
-        soup = BeautifulSoup(html.text, 'html.parser')
+        soup = BeautifulSoup(html.text, "html.parser")
         url_suffixes = [
             u["href"] for u in soup.find_all("a") if "dictionary/words/" in u["href"]
         ]
@@ -169,14 +169,14 @@ def parse_information(html) -> Word:
     """
     Returns a tuple of the word and the info for the word.
     """
-    soup = BeautifulSoup(html.text, 'html.parser')
+    soup = BeautifulSoup(html.text, "html.parser")
 
     # Get the word
     word = soup.find_all("em")[0].string
 
     # Get the keywords
     keywords_div = soup.find("div", {"id": "keywords"})
-    keywords_lines = [l.lstrip() for l in keywords_div.text.split('\n')][4:]
+    keywords_lines = [l.lstrip() for l in keywords_div.text.split("\n")][4:]
     keywords = [k.rstrip(",") for k in keywords_lines if k]
 
     # Get the video links
@@ -209,6 +209,9 @@ async def main():
     # Get the URLs for all the word pages.
     if args.urls:
         urls = args.urls
+    elif args.urls_file:
+        with open(args.urls_file, "r") as f:
+            urls = f.read().splitlines()
     else:
         urls = await get_word_page_urls(executor, letters=args.letters)
 
