28fbfc070fae1c8dd57a40a97d05f6c4df3f528b
Daniel Porteous <danielporteous1@gmail.com>
2020-09-10 22:47:53 +1000

    [lib] Show keywords

--- lib/word_page.dart
+++ lib/word_page.dart
@@ -10,27 +10,28 @@ class WordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(word.word),
-        ),
-        body: VideoPlayerScreen(videoLink: word.videoLinks[0]));
+      appBar: AppBar(
+        title: Text(word.word),
+      ),
+      // TODO: Don't just show first video, make a video swiper thingo.
+      body: VideoPlayerScreen(word: word),
+    );
   }
 }
 
 class VideoPlayerScreen extends StatefulWidget {
-  VideoPlayerScreen({Key key, this.videoLink}) : super(key: key);
+  VideoPlayerScreen({Key key, this.word}) : super(key: key);
 
-  final String videoLink;
+  final Word word;
 
   @override
-  _VideoPlayerScreenState createState() =>
-      _VideoPlayerScreenState(videoLink: videoLink);
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState(word: word);
 }
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  _VideoPlayerScreenState({this.videoLink});
+  _VideoPlayerScreenState({this.word});
 
-  final String videoLink;
+  final Word word;
 
   VideoPlayerController _controller;
   Future<void> _initializeVideoPlayerFuture;
@@ -41,7 +42,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     // offers several different constructors to play videos from assets, files,
     // or the internet.
     _controller = VideoPlayerController.network(
-      videoLink,
+      word.videoLinks[0],
     );
 
     // Initialize the controller and store the Future for later use.
@@ -69,29 +70,39 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     return Scaffold(
       // Use a FutureBuilder to display a loading spinner while waiting for the
       // VideoPlayerController to finish initializing.
-      body: FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the VideoPlayerController has finished initialization, use
-            // the data it provides to limit the aspect ratio of the video.
-            return AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              // Use the VideoPlayer widget to display the video.
-              child: VideoPlayer(_controller),
-            );
-          } else {
-            // If the VideoPlayerController is still initializing, show a
-            // loading spinner.
-            return Padding(
-                padding: EdgeInsets.only(top: 100),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [CircularProgressIndicator()],
-                ));
-          }
-        },
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the VideoPlayerController has finished initialization, use
+                  // the data it provides to limit the aspect ratio of the video.
+                  return AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    // Use the VideoPlayer widget to display the video.
+                    child: VideoPlayer(_controller),
+                  );
+                } else {
+                  // If the VideoPlayerController is still initializing, show a
+                  // loading spinner.
+                  return Padding(
+                      padding: EdgeInsets.only(top: 100),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [CircularProgressIndicator()],
+                      ));
+                }
+              },
+            ),
+            Padding(
+              child: Text("Keywords: ${word.keywords.join(', ')}"),
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+            ),
+          ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Wrap the play or pause in a call to `setState`. This ensures the
