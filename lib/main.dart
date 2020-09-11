02e8606f91de12c01651a277cf254786f239f32f
Daniel Porteous <danielporteous1@gmail.com>
2020-09-11 14:15:24 +1000

    [lib] Don't show debug ribbon

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Auslan Dictionary',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           // This is the theme of your application.
           //
