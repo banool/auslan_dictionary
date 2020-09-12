0864379f8bb76391dff268216d092d771e01949e
Daniel Porteous <danielporteous1@gmail.com>
2020-09-12 12:03:39 +1000

    [iOS] Set up CI/CD with fastlane

---
+++ ios/publish.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+[[ -z "${FASTLANE_USER}" ]] && echo 'Please set FASTLANE_USER' && exit
+[[ -z "${FASTLANE_PASSWORD}" ]] && echo 'Please set FASTLANE_PASSWORD' && exit
+[[ -z "${FASTLANE_SESSION}" ]] && echo 'Please set FASTLANE_SESSION' && exit
+[[ -z "${FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD}" ]] && echo 'Please set FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD' && exit
+[[ -z "${MATCH_KEYCHAIN_NAME}" ]] && echo 'Please set MATCH_KEYCHAIN_NAME' && exit
+[[ -z "${MATCH_KEYCHAIN_PASSWORD}" ]] && echo 'Please set MATCH_KEYCHAIN_PASSWORD' && exit
+[[ -z "${MATCH_PASSWORD}" ]] && echo 'Please set MATCH_PASSWORD' && exit
+
+bundle exec fastlane ios beta
