97b6e23ff95920c3e6c6b41c4180503b69b05eb3
Daniel Porteous <danielporteous1@gmail.com>
2021-04-26 14:34:27 -0700

    [scripts] Add script that gets words one letter at a time

---
+++ scripts/incremental_scrape.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Scrapes the signbank one letter at a time.
+# Optionally accepts a positional argument, a regex for the letters.
+# For example, if you only want to do from d onwards, you could run:
+#   ./incremental_scrape.sh '[d-z]'
+# This allows you to scrape a few letters at a time without losing progress.
+
+if [ -z "$PIP_PYTHON_PATH" ]
+then
+    echo "ERROR: Run this from within the pipenv."
+    exit 1
+fi
+
+REGEX=$1
+
+cd "$(dirname "$0")"
+
+if [[ -z $REGEX ]]; then
+    echo "No regex was passed, copying in current data"
+    cp ../assets/data/words.json previous.json
+else
+    echo "Regex was passed, using current previous.json"
+fi
+
+rm -f letters_retrieved.txt
+
+for l in {a..z}
+do
+    if [[ -z $REGEX || $l =~ $REGEX ]]; then
+        echo "Getting words for letter $l"
+        python scrape_signbank.py -d --output-file next.json --existing-file previous.json --letter $l
+        echo "Successfully got words for letter $l"
+        mv next.json previous.json
+        echo $l >> letters_retrieved.txt
+    fi
+done
+
+mv previous.json all_letters.json
+
+echo "Scraped all letters, now make sure all_letters.json looks good and move it to ../assets/data/words.json if it does"
+
