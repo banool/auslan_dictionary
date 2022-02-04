7988327e082a8c48ffd7025e02253d3a83f5c607
Daniel Porteous <danielporteous1@gmail.com>
2022-02-04 11:41:16 +1100

    Add script to make data update PR

---
+++ scripts/create_data_update_pr.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set +e
+
+if [ -z "$PIP_PYTHON_PATH" ]
+then
+    echo "ERROR: Run this from within the pipenv."
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+
+# Go to top level directory.
+cd ..
+
+# Forcibly make repo match remote.
+git fetch
+git reset --hard origin/master
+
+# Delete PR and branch if it already exists.
+existingpr=`gh pr list --label dataupdate --json number | jq -r .[0]`
+if [ "$existingpr" != "null" ]; then
+    gh pr close $existingpr -d 
+fi
+
+# Switch to a new branch.
+git checkout -b update_data
+
+# Scrape for new data.
+scripts/incremental_scrape.sh
+
+# Move data into place.
+scripts/move_data.sh
+
+# Create and push the commit.
+git add -A
+git commit -m "Update signbank data"
+git push --set-upstream origin update_data
+
+# Make a PR with the commit.
+gh pr create --fill --label dataupdate
+
+# Switch back to master.
+git checkout master
+
+# Delete branch.
+git branch -d update_data
+
+echo 'Done!'
