58e3fe72ae7e3ee4dd88b4bb0d733105c49e1df4
Daniel Porteous <danielporteous1@gmail.com>
2022-02-16 19:22:33 -0800

    Improve error handling for create PR script

--- scripts/create_data_update_pr.sh
+++ scripts/create_data_update_pr.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-set +e
+set -e
+
+# Putting ||: at the end of a command means the script won't abort if that one
+# command fails.
 
 onexit() {
     # Switch back to master.
     git checkout master
 
     # Delete branch.
-    git branch -D update_data
+    git branch -D update_data ||:
 }
 
 if [ -z "$PIP_PYTHON_PATH" ]
@@ -24,6 +27,7 @@ cd ..
 # Forcibly make repo match remote.
 git fetch
 git reset --hard origin/master
+git checkout master
 
 # Delete PR if it already exists.
 existingpr=`gh pr list --label data_update --json number | jq -r .[0].number`
@@ -32,8 +36,8 @@ if [ "$existingpr" != "null" ]; then
 fi
 
 # Delete branch if it already exists.
-git push -d origin update_data
-git branch -D update_data
+git push -d origin update_data ||:
+git branch -D update_data ||:
 
 # Switch to a new branch.
 git checkout -b update_data
@@ -42,8 +46,7 @@ git checkout -b update_data
 scripts/incremental_scrape.sh
 
 # Exit if nothing changed.
-diff all_letters.json ../assets/data/words.json > /dev/null
-if [ $? -eq 0 ]; then
+if diff all_letters.json ../assets/data/words.json > /dev/null; then
     echo "No new data, exiting..."
     onexit
     exit 1
