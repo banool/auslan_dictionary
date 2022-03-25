dddd4353e4435532473ed02e2316c00621c246db
Daniel Porteous <danielporteous1@gmail.com>
2022-03-24 17:19:41 -0700

    Take revision out of beta

--- lib/main.dart
+++ lib/main.dart
@@ -218,7 +218,7 @@ class _MyHomePageState extends State<MyHomePage> {
             label: "Revision",
           ),
           FlashcardsLandingPage(controller: flashcardsPageController),
-          "Revision (Beta)"));
+          "Revision"));
     }
 
     information.add(TabInformation(
